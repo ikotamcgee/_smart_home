b4354b20c6c3d7852a42b202611464944e6e23f6
Maksim Terekhov <ikotamcgee@gmail.com>
2026-01-28 13:49:55 +0900

    added thermometer and socket functionality

--- src/devices/socket.rs
+++ src/devices/socket.rs
@@ -1,1 +1,33 @@
+pub struct Socket {
+    is_on: bool,
+    power: f64,
+}
 
+impl Socket {
+    fn new() -> Self {
+        Self {
+            is_on: false,
+            power: 0.0,
+        }
+    }
+
+    fn turn_on(&mut self) {
+        self.is_on = true
+    }
+
+    fn turn_off(&mut self) {
+        self.is_on = false
+    }
+
+    fn is_on(&self) -> bool {
+        self.is_on
+    }
+
+    fn set_power(&mut self, value: f64) {
+        self.power = value
+    }
+
+    fn power(&self) -> f64 {
+        if self.is_on { self.power } else { 0.0 }
+    }
+}
--- src/devices/thermometer.rs
+++ src/devices/thermometer.rs
@@ -1,1 +1,17 @@
+pub struct Thermometer {
+    temperature: f64,
+}
 
+impl Thermometer {
+    fn new() -> Self {
+        Self { temperature: 0.0 }
+    }
+
+    fn temperature(&self) -> f64 {
+        self.temperature
+    }
+
+    fn set_temperature(&mut self, value: f64) {
+        self.temperature = value
+    }
+}
