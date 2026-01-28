695255a278e174e1509726b9e9639a4e11056941
Maksim Terekhov <ikotamcgee@gmail.com>
2026-01-28 17:30:18 +0900

    1# homework

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,4 @@ use crate::devices::{Device, socket::Socket, thermometer::Thermometer};
 mod devices;
 mod home;
 
-pub fn app() {
-    let mut t1 = Thermometer::new("Домашний");
-    t1.set_temperature(24.5);
-
-    let mut s1 = Socket::new("Алиса");
-    s1.set_power(60.0);
-
-    let dev1 = Device::Thermometer(t1);
-    let dev2 = Device::Socket(s1);
-
-    dev1.report();
-    dev2.report();
-}
+pub fn app() {}
